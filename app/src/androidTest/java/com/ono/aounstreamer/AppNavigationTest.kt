d13d99e49879e069f4596216b3160768c2cf0a88
Muhammad Aoun <51170716+MuhammadAounAnwar@users.noreply.github.com>
2024-11-02 07:39:36 +0500

    AppNavigationTest.kt one test passed.

--- app/src/androidTest/java/com/ono/aounstreamer/AppNavigationTest.kt
+++ app/src/androidTest/java/com/ono/aounstreamer/AppNavigationTest.kt
@@ -3,19 +3,13 @@ package com.ono.aounstreamer
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.PagingData
 import com.ono.aounstreamer.util.AppNavigation
 import com.ono.streamerlibrary.domain.model.MediaItem
 import com.ono.streamerlibrary.domain.usecase.GetMediaItemsUseCase
-import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.mockk
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
@@ -25,7 +19,6 @@ class AppNavigationTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-//    private val viewModel = mockk<MainViewModel>(relaxed = true)
 
     private val testUseCase = TestGetMediaItemsUseCase()
     private val viewModel = MainViewModel(testUseCase)
@@ -47,61 +40,6 @@ class AppNavigationTest {
         composeTestRule.onNodeWithContentDescription("SearchBar").assertIsDisplayed()
     }
 
-
-    @Test
-    fun clickingItemNavigatesToDetailScreen() = runTest {
-        val testUseCase = object : GetMediaItemsUseCase {
-            override suspend fun invoke(query: String): Flow<PagingData<MediaItem>> {
-                val mediaItemList = PagingData.from(listOf(mediaItem))
-                return flow { emit(mediaItemList) }
-            }
-        }
-
-        val viewModel = MainViewModel(testUseCase)
-
-        composeTestRule.setContent {
-            AppNavigation(navController = rememberNavController(), viewModel = viewModel)
-        }
-
-        composeTestRule.onNodeWithContentDescription("MediaCard")
-            .assertIsDisplayed()
-
-        composeTestRule.onNodeWithContentDescription("MediaCard")
-            .performClick()
-
-        composeTestRule.onNodeWithContentDescription("Item Title")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun detailScreenNavigatesToPosterScreen() {
-        every { viewModel.selectedItem } returns mediaItem
-
-        composeTestRule.setContent {
-            val navController = rememberNavController()
-            AppNavigation(navController = navController, viewModel = viewModel)
-        }
-
-        composeTestRule.onNodeWithText("Watch Poster").performClick()
-
-        composeTestRule.onNodeWithText("Poster Screen Title")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun detailScreenNavigatesToPlayerScreen() {
-        every { viewModel.selectedItem } returns mediaItem
-
-        composeTestRule.setContent {
-            val navController = rememberNavController()
-            AppNavigation(navController = navController, viewModel = viewModel)
-        }
-
-        composeTestRule.onNodeWithText("Watch Video").performClick()
-
-        composeTestRule.onNodeWithText("Player Screen Title")
-            .assertIsDisplayed()
-    }
 }
 
 class TestGetMediaItemsUseCase : GetMediaItemsUseCase {
